01c25cd5bc5df420517b6751992055a28983b6d8
Emanuele Coppola <emanuele.coppola99@gmail.com>
2024-04-30 08:33:52 +0200

    Fix comment typo

--- src/Services/HTTPService.php
+++ src/Services/HTTPService.php
@@ -195,7 +195,7 @@ class HTTPService extends BaseService {
 
         $signature = [];
 
-        // fyi: using array_merge instead of spread because spread is not supported in PHP >= 7.4
+        // fyi: using array_merge instead of spread because spread is not supported in PHP < 7.4
         foreach(array_merge($headers, $signatureHeaders) as $header => $value) {
             $signature[]                = mb_strtolower($header) . ': ' . $value;
             $signatureHeadersSequence[] = $header;
