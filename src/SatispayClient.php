221aaf15580799408c3a0443889f33245a849b2b
Emanuele Coppola <EmanueleCoppola@users.noreply.github.com>
2024-06-02 16:46:15 +0000

    add: RSAService auto-resolution

--- src/SatispayClient.php
+++ src/SatispayClient.php
@@ -2,8 +2,12 @@
 
 namespace EmanueleCoppola\Satispay;
 
+use EmanueleCoppola\Satispay\Contracts\RSAServiceContract;
+use EmanueleCoppola\Satispay\Exceptions\SatispayRSAException;
 use EmanueleCoppola\Satispay\Services\AuthenticationService;
 use EmanueleCoppola\Satispay\Services\HTTPService;
+use EmanueleCoppola\Satispay\Services\RSAService\OpenSSL_RSAService;
+use EmanueleCoppola\Satispay\Services\RSAService\SeclibRSAService;
 
 /**
  * Class SatispayClient
@@ -27,6 +31,9 @@ class SatispayClient {
 
         //
         'sandbox' => false,
+
+        //
+        'rsa_service' => null,
 
         // HTTP
         'psr' => [],
@@ -53,6 +60,13 @@ class SatispayClient {
      * @var AuthenticationService
      */
     public $authentication;
+
+    /**
+     * RSA service instance for managing RSA keys.
+     *
+     * @var RSAServiceContract
+     */
+    public $rsa_service;
 
     //
     const STAGING_BASE_URL = 'https://staging.authservices.satispay.com';
@@ -85,6 +99,16 @@ class SatispayClient {
             $this->config['psr']
         );
 
+        // RSA Service resolution
+        if (
+            $this->config['rsa_service'] &&
+            $this->config['rsa_service'] instanceof RSAServiceContract
+        ) {
+            $this->rsa_service = $this->config['rsa_service'];
+        } else {
+            $this->resolveRSAService();
+        }
+
         $this->authentication = new AuthenticationService(
             $this,
             $this->config['public_key'],
@@ -102,6 +126,33 @@ class SatispayClient {
      * @return void
      */
     protected function boot() {}
+
+    /**
+     * Resolve the RSA service to be used.
+     * OpenSSL will be used if the OpenSSL extension is available.
+     * The phpseclib will be used if found in the composer.json.
+     *
+     * @throws SatispayRSAException if non of the services is available.
+     *
+     * @return void
+     */
+    private function resolveRSAService()
+    {
+        foreach([OpenSSL_RSAService::class, SeclibRSAService::class] as $RSAservice) {
+
+            /** @var RSAServiceContract */
+            $RSAserviceInstance = new $RSAservice;
+
+            if ($RSAserviceInstance->isAvailable()) {
+                $this->rsa_service = $RSAserviceInstance;
+                break;
+            }
+        }
+
+        if (!$this->rsa_service) {
+            throw new SatispayRSAException('No RSA Service available. Either install the OpenSSL PHP extension or the phpseclib 3 composer library.');
+        }
+    }
 
     /**
      * Get the current configuration settings for the SatispayClient.
