b236e1bf6100c0cf1afe815ff548b138f0bff7c7
Emanuele Coppola <EmanueleCoppola@users.noreply.github.com>
2024-06-02 16:45:34 +0000

    add: SatispayRSAException

---
+++ src/Exceptions/SatispayRSAException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EmanueleCoppola\Satispay\Exceptions;
+
+use Exception;
+
+/**
+ * Class SatispayRSAException
+ *
+ * Represents an exception specific to the Satispay RSA generation.
+ */
+class SatispayRSAException extends Exception {}
